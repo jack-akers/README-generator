c93c986388724fff7afa2b2a447eed66cd07d4d7
Jack Akers <akers.jack@outlook.com>
2023-06-15 18:52:19 -0400

    questions done

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,68 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'Please describe your project:',
+        name: 'desc',
+      },
+      {
+        type: 'input',
+        message: 'Please provide intallation instructions for your project:',
+        name: 'install',
+      },
+      {
+        type: 'input',
+        message: 'Please provide details on how to use your project:',
+        name: 'use',
+      },
+      {
+        type: 'input',
+        message: 'Did anyone else contribute to your project?',
+        name: 'contrib',
+      },
+      {
+        type: 'list',
+        message: 'Which license did you use for your project?',
+        choices: [
+            "MIT",
+            "Apache-2.0",
+            "GPL-3.0",
+            "BSD-3-Clause",
+            "Unlicense",
+            "Mozilla Public License 2.0",
+            "GNU AGPLv3",
+            "GNU GPLv3",
+            "GNU LGPLv3",
+            "Eclipse Public License 1.0",
+            "GNU FDL",
+            "ISC",
+            "Creative Commons Zero v1.0 Universal",
+            "Other",
+          ],
+        name: 'license',
+      },
+      {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'gitName',
+      },
+      {
+        type: 'input',
+        message: 'What is your e-mail address?',
+        name: 'email',
+      },
+  
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
