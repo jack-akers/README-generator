4a9115e2f5d7999f315721ecc3e127d070f3e35b
Jack Akers <akers.jack@outlook.com>
2023-06-21 20:10:13 -0400

    functionality

--- Develop/index.js
+++ Develop/index.js
@@ -2,6 +2,7 @@
 
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { error } = require('console');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -65,10 +66,25 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>{
+        if (err){
+            console.error(err);
+        } else {
+            console.log(`README.md successfully generated as ${fileName}`);
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt (questions)
+    .then((answers) => {
+        const markdown = generateMarkdown.generateMarkdown(answers);
+        writeToFile("README.md", markdown);
+    });
+}
 
 // Function call to initialize app
 init();
